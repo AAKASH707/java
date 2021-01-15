627f78a0d46d502cd228b00f4ec29c78848317dd
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2021-01-15 12:32:02 +0530

    Create tableofnumber.java

---
+++ JavaLab/exp1/tableofnumber.java
@@ -0,0 +1,14 @@
+// beaware of class name while running the file
+public class prog1 {
+
+	public static void main(String arg[]) 
+	{	
+		int i=Integer.parseInt(arg[0]);
+		for(int k=1;k<=10;k++)
+		{	
+			System.out.println(i+"*"+k+"="+(i*k));
+		}
+		
+	}
+
+}
